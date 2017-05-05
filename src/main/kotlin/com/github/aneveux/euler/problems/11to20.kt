5d294fc81ca59d65cae5235cfc9056d4364559e7
Antoine Neveux <antoine.neveux@gmail.com>
2017-05-05 18:52:27 +0200

    Preparing for 11 :)

---
+++ src/main/kotlin/com/github/aneveux/euler/problems/11to20.kt
@@ -0,0 +1,13 @@
+package com.github.aneveux.euler.problems
+
+import com.github.aneveux.euler.Problem
+
+/**
+ * Solving [https://projecteuler.net/problem=11]
+ *
+ * > What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+ *
+ */
+class Problem11 : Problem() {
+    override fun solve() = TODO()
+}
