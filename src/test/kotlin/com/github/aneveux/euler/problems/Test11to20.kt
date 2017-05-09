c7a4ea8a3fe2ba1e12a3dda2ea212ee99404910c
Antoine Neveux <antoine.neveux@gmail.com>
2017-05-09 16:02:21 +0200

    Solved 11.

--- src/test/kotlin/com/github/aneveux/euler/problems/Test11to20.kt
+++ src/test/kotlin/com/github/aneveux/euler/problems/Test11to20.kt
@@ -1,2 +1,13 @@
 package com.github.aneveux.euler.problems
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class TestProblem11 {
+    val problem = Problem11()
+
+    @Test
+    fun testProblem11Solution() {
+        assertThat(problem.solve()).isEqualTo(70600674.toString())
+    }
+}
