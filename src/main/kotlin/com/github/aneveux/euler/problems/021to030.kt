15187b4f3c6131aaf98a5acade5aa3555df4be3c
Antoine Neveux <antoine.neveux@worldline.com>
2017-08-02 17:58:38 +0200

    Prepared for 27.

--- src/main/kotlin/com/github/aneveux/euler/problems/021to030.kt
+++ src/main/kotlin/com/github/aneveux/euler/problems/021to030.kt
@@ -102,3 +102,13 @@ class Problem26 : Problem() {
     override fun solve() = (1..999).mapIndexed { index, i -> index + 1 to multiplicativeOrder(i) }
             .maxBy { (_, v) -> v }?.first.toString()
 }
+
+/**
+ * Solving [https://projecteuler.net/problem=27]
+ *
+ * > Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n=0.
+ *
+ */
+class Problem27 : Problem() {
+    override fun solve() = ""
+}
